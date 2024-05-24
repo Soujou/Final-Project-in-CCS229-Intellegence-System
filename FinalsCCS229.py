ecb0dba016b39a7cef9385e0fd2bd90ebbfb239e
Angelica Villar <99379989+Soujou@users.noreply.github.com>
2024-05-25 01:37:08 +0800

    Add files via upload

--- FinalsCCS229.py
+++ FinalsCCS229.py
@@ -2,8 +2,21 @@
 #Final Project in CCS 229 - Intelligent Systems
 
 import streamlit as st
-import openai
+from openai import OpenAI
 
+from openai import OpenAI
+
+client = OpenAI()
+
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
+        
 #OpenAI API key
 openai.api_key = "FINALPROJECTCCS229"
 
